fixing bug in gravity calculation that affected concave shapes (i.e., contact binaries). Thank you Toshi!!!

example/surface_slopes.py
@@ -54,10 +54,10 @@ a_mag = np.linalg.norm(a_net, axis=1)[:, np.newaxis]
 slopes = np.degrees(np.arccos(np.sum(-normals*a_net/a_mag,axis=1)))
 
 
-
+print("maximum slope:", np.max(slopes))
 
 cmap = 'plasma'
-vmax=90
+vmax=75
 norm = mpl.colors.Normalize(vmin=0.0, vmax=vmax)
 mappable = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
 colors = mpl.colormaps.get_cmap(cmap)(norm(slopes))[:,:3]
@@ -88,7 +88,7 @@ for tick in cbar.get_ticks():
     tickLabels += [r"${0}^{{\circ}}$".format(tick),]
 cbar.set_ticklabels(tickLabels)
 
-
+azim, elev = -140, 20
 ax.view_init(azim=-140.0, elev=20.0)
 
 ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
@@ -97,11 +97,38 @@ ax.zaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
 
 plt.tight_layout()
 plt.savefig('slopes.png')
-plt.close()
-
-
-
 print("3D surface slope plot saved to slopes.png")
+# plt.close()
+
+
+# a_normal = np.sum(a_net*normals,axis=1) #normal component magnitude
+# a_hor = a_net - a_normal.reshape(-1,1)*normals #subtract off normal component
+# a_hor_norm = a_hor/np.linalg.norm(a_hor,axis=1).reshape(-1,1) #acceleration parallel to surface, normalized
+# nskip = 10
+# arrow_length = 75
+# distance = 1e6  # assume observer is far away 
+# camera_position = np.array([
+#     distance * np.cos(np.radians(elev)) * np.cos(np.radians(azim)),  # X
+#     distance * np.cos(np.radians(elev)) * np.sin(np.radians(azim)),  # Y
+#     distance * np.sin(np.radians(elev))                       # Z
+# ])
+
+# arrow_pos = face_midpoints[::nskip,:]
+# arrow_dir = a_hor_norm[::nskip,:]
+# arrow_dir /= np.linalg.norm(arrow_dir,axis=1).reshape(-1,1)
+# arrow_norms = normals[::nskip,:]
+# arrow_pos += arrow_norms*20
+
+# to_arrows = arrow_pos - camera_position
+# dots = np.sum(arrow_norms*to_arrows, axis=1)
+# visible_arrows = np.where(dots<0.0)[0]
+# x,y,z = arrow_pos[visible_arrows,0], arrow_pos[visible_arrows,1], arrow_pos[visible_arrows,2]
+# u,v,w = arrow_dir[visible_arrows,0], arrow_dir[visible_arrows,1], arrow_dir[visible_arrows,2]
+
+# quiver_plot=ax.quiver(x,y,z,u,v,w, length=arrow_length, normalize=False, color='w')#, zorder=10)
+# plt.savefig('slopes_and_direction.png', dpi=300)
+
+
 
 
 

polygrav.py
@@ -283,7 +283,6 @@ def recenter_shape(verts,faces, align_principal_axes=True):
         eigVal = eigVal[order]
         eigVec = eigVec[:,order]
         verts = np.dot(verts, eigVec)
-
     return verts
 
 def recenter_shape_porter(verts,faces, vertex_normals, align_principal_axes=True):
@@ -349,6 +348,69 @@ def recenter_shape_porter(verts,faces, vertex_normals, align_principal_axes=True
         vertex_normals = np.dot(vertex_normals, eigVec)
 
     return verts, vertex_normals
+
+
+def compute_principal_axes(verts, faces):
+    #compute the offset and rotation matrix that would rotate a shape model to principal axis alignment
+
+    rho = 1 #doesn't actually matter, density can be any constant
+    P = np.zeros((3,3))
+    for j in range(0,3):
+        for k in range(0,3):
+            V = 0.0
+            R = np.zeros(3)
+            for i in range(0,len(faces[:,0])):
+                D = verts[faces[i,0]]
+                E = verts[faces[i,1]]
+                F = verts[faces[i,2]]
+
+                G = E-D
+                H = F-D
+                N = np.cross(G,H)
+                dV = np.abs(np.dot(D/3.0,N/2.0))
+
+                dR = (D+E+F)/4.0
+                V += dV
+                R += dV*dR
+
+                P[j,k] += (rho*dV/20.) * (2.*D[j]*D[k] + 2.*E[j]*E[k] + 2.*F[j]*F[k] 
+                                         + D[j]*E[k] + D[k]*E[j] 
+                                         + D[j]*F[k] + D[k]*F[j]
+                                         + E[j]*F[k] + E[k]*F[j])
+    R = R/V
+
+
+    offset = R #want to return this
+
+
+    M = rho*V
+    X, Y, Z = R
+    #make inertia tensor:
+    Ixx = np.sum(np.diag(P)) - P[0,0]
+    Iyy = np.sum(np.diag(P)) - P[1,1]
+    Izz = np.sum(np.diag(P)) - P[2,2]
+    Ixy = -P[0,1]
+    Ixz = -P[0,2]
+    Iyz = -P[1,2]
+
+    I = np.array([[Ixx, Ixy, Ixz],
+                  [Ixy, Iyy, Iyz],
+                  [Ixz, Iyz, Izz]])
+
+    I -= M*np.array([[Y**2+Z**2, -X*Y, -X*Z],
+                     [-X*Y, X**2+Z**2, -Y*Z],\
+                     [-X*Z, -Y*Z, X**2+Y**2]])
+
+    #rotate verts to principal axis alignment
+    eigVal, eigVec = np.linalg.eig(I)
+    order = np.argsort(eigVal)
+    eigVal = eigVal[order]
+    eigVec = eigVec[:,order]
+    # verts = np.dot(verts, eigVec)
+    return offset, eigVec
+
+
+
 
 def polygrav_slow(r_field, sigma, verts, faces, edges, faces_on_edge, G=6.67430e-11):
     #dont use this....